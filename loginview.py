c9186e304d67841b25ee798797a10039d8d33d12
SarahL88 <sarahlei@bu.edu>
2018-03-27 09:49:38 -0400

    Changed input data format. Instead of user giving csv files, PLite processes gb files and writes csv files.

--- loginview.py
+++ loginview.py
@@ -3,6 +3,7 @@ import process_input_data
 import make_constellation_request
 import make_puppeteer_request
 import os
+from shutil import rmtree
 import sys
 import uuid
 from Bio import SeqIO
@@ -14,12 +15,13 @@ CONCENTRATION_UNIT = 'NANOGRAMS_PER_MICROLITER'
 VOLUME_UNIT = 'MICROLITERS'
 
 
-def login_view():
+#def login_view():
+def login_view(zip, numdesigns):
     authorid = str(uuid.uuid4())
     instanceid = str(uuid.uuid4())
     date = datetime.date.today()
-    ZIPFILE = sys.argv[1]
-    NUMDESIGNS = sys.argv[2]
+    ZIPFILE = zip #sys.argv[1]
+    NUMDESIGNS = numdesigns #sys.argv[2]
 
     # Parse input files, populate 'repo' dict
     repo = process_input_data.make_repo(ZIPFILE, instanceid, authorid, date)
@@ -34,6 +36,24 @@ def login_view():
     write_gb_files(NUMDESIGNS, gb_records)
 
     # Write request.json file
+    write_json_file(request)
+
+
+
+def write_gb_files(NUMDESIGNS, gb_records):
+    now = datetime.datetime.now()
+    gb_directory = now.strftime("%Y-%m-%d") + "-" + str(NUMDESIGNS) + "GB-Sequences"
+    if os.path.exists(gb_directory):
+        rmtree(gb_directory)
+    os.mkdir(gb_directory)
+    ctr = 0
+    for gb in gb_records:
+        gb_file = "./" + gb_directory + '/Design_' + str(ctr) + '.gb'
+        output_file = open(gb_file, 'w')
+        SeqIO.write(gb, output_file, 'genbank')
+        ctr += 1
+
+def write_json_file(request):
     orig_stdout = sys.stdout
     f = open('request.json', 'w')
     sys.stdout = f
@@ -43,15 +63,5 @@ def login_view():
 
     print('Front end printed results to request.json')
 
-
-def write_gb_files(NUMDESIGNS, gb_records):
-    gb_directory = str(NUMDESIGNS) + "_GB_Sequences"
-    os.mkdir(gb_directory)
-    ctr = 0
-    for gb in gb_records:
-        gb_file = "./" + gb_directory + '/Design_' + str(ctr) + '.gb'
-        output_file = open(gb_file, 'w')
-        SeqIO.write(gb, output_file, 'genbank')
-        ctr += 1
-
-login_view()
+#login_view()
+login_view('HeadtoHead2New.zip', 3)
